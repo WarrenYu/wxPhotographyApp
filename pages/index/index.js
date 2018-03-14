c3245dfb184bf4ecf4df0ea1012cc28208d76293
Warren <yuguopeng900@163.com>
2018-03-15 01:29:35 +0800

    @Warren: 在首页顶部添加轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,16 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    data: {
+        imgUrls: [
+            'http://www.superwarren.top/wp-content/uploads/2018/03/wxApp1-1.jpg',
+            'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp2.jpg',
+            'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp3.jpg',
+            'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp4-1.jpg'
+        ],
+        indicatorDots: true,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
