7e396a2ce6f229250f30c7f40cd96a1efc53d99a
Warren Yu <yuguopeng900@163.com>
2018-03-19 00:17:47 +0800

    @Warren: 设计页面中部图标，并设置好布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,24 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+
 
 Page({
     data: {
-        imgUrls: [
-            'http://www.superwarren.top/wp-content/uploads/2018/03/wxApp1-1.jpg',
-            'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp2.jpg',
-            'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp3.jpg',
-            'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp4-1.jpg'
-        ],
-        indicatorDots: true,
-        autoplay: true,
-        interval: 5000,
-        duration: 1000
+        swiper:{
+            imgUrls: [
+                'http://www.superwarren.top/wp-content/uploads/2018/03/wxApp1-1.jpg',
+                'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp2.jpg',
+                'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp3.jpg',
+                'https://www.superwarren.top/wp-content/uploads/2018/03/wxApp4-1.jpg'
+            ],
+            indicatorDots: true,
+            autoplay: true,
+            interval: 5000,
+            duration: 1000
+        }
+    },
+    navigateToPicturePage: function () {
+        wx.navigateTo({ url: '../pictureWall/pictureWall'})
     }
 })
